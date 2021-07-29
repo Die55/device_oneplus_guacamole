a7f46416477a6ddcb051414690148822b31e5ed6
Die55 <diego_albense@hotmail.com>
2021-07-29 16:08:25 +0530

    fix oneplus launcher and remove battery stats

--- OPLauncher/oplauncher.mk
+++ OPLauncher/oplauncher.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := device/xiaomi/raphael/OPLauncher
+LOCAL_PATH := device/oneplus/guacamole/OPLauncher
 
 PRODUCT_PACKAGES += \
     OPLauncherReleases \
